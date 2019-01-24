18d99ef3b1537494300809d167480ef88d31af31
Michael Wang <wangshiy@oregonstate.edu>
2019-01-24 10:42:00 +0800

    The initial version of Team 2 Project

---
+++ MarioPlatformer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame (LateUpdate runs after Upadte
+	void LateUpdate () 
+	{
+		transform.position = player.transform.position + offset;	
+	}
+}
